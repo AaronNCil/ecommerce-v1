d17a7544001c22cd1828cc9e1afc97c1c9224cea
Aaron Cil <cilaaron27@gmail.com>
2022-10-03 16:47:24 -0400

    changed image sizes, linked json file to front end, styles products

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-let http = new XMLHttpRequest();
+let request = new XMLHttpRequest();
 
-http.open('get', 'products.json', true);
+request.open('get', 'products.json', true);
 
-http.send();
+request.send();
 
-http.onload = function(){
-   if (this.readyState == 4 && this.status == 200){
-      let products = JSON.parse(this.responseText);
-   }
+request.onload = function(){
 
+   let products = JSON.parse(this.responseText);
 
    let output = "";
 
@@ -25,7 +23,7 @@ http.onload = function(){
           <hr>
           <p class="desc"><i>${item.desc}</i></p>
       </div>
-      <label><b>Price :</b>${item.price}</label>
+      <label><b>Price: </b>${item.price}</label>
       <div class="btn-2">Add to cart</div>
   </section>
       `
@@ -33,83 +31,6 @@ http.onload = function(){
 
    document.querySelector(".cards-container").innerHTML = output;
 }
-
-let products = 
-[
-  {
-     "image": "images/airpodmax.jpg",
-     "name": "Airpod Pro Maxes",
-     "desc": "Noise cancellation headphones best quality with an even better style",
-     "price":"$550"
-  },
-  {
-      "image": "images/prodimg1.jpg",
-      "name": "Beats Solo3",
-      "desc": "Noise cancellation headphones best quality with an even better style",
-      "price":"$131"
-  },
-  {
-      "image": "images/prodimg2.jpg",
-      "name": "Koss Prota Pro",
-      "desc": "Noise cancellation headphones best quality with an even better style",
-      "price":"$50"
-  },
-  {
-   "image": "images/prodimg3.png",
-   "name": "Steel Series Arctis 3",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$70"
-  },
-  {
-   "image": "images/prodimg4.png",
-   "name": "Hyper X Cloud II",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$70"
-  },
-  {
-   "image": "images/prodimg5.png",
-   "name": "Bose Quietcomfort",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$229.00"
-  },
-  {
-   "image": "images/prodimg6.png",
-   "name": "Razer Kraken X",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$550"
-  },
-  {
-   "image": "images/prodimg7.png",
-   "name": "Sony wh- 1000xm5",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$400"
-  },
-  {
-   "image": "images/prodimg8.png",
-   "name": "JBL Tune 510BT ",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"50"
-  },
-  {
-   "image": "images/prodimg9.png",
-   "name": "Marshall Major IV",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$107"
-  },
-  {
-   "image": "images/prodimg10.png",
-   "name": "Sennheiser Momentum 4 Wireless",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$350"
-  },
-  {
-   "image": "images/prodimg11.png",
-   "name": "Bang & Olufsen",
-   "desc": "Noise cancellation headphones best quality with an even better style",
-   "price":"$500"
-  }
-]
-
 
 
 // image slider
@@ -129,16 +50,6 @@ let images = [
 function startUp() {
   document.getElementById('image1').src = images[0].imageUrl;
 }
-
-// let globalIndex = 0;
-
-// function next() {
-//     globalIndex++;
-//   if (globalIndex > images.length) {
-//     globalIndex = 0;
-//   }
-//   document.getElementById('image1').src = images[globalIndex].imageUrl;
-// }
 
 function chooseImage(imageIndex) {
   document.getElementById('image1').src = images[imageIndex].imageUrl;
